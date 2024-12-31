45316c0cd1edf987eee96febcb9f5815f47b95c3
Tamanna Sultana <nigahtamanna123@gmail.com>
2024-12-31 16:27:45 +0600

    commit

---
+++ dummydata.sql
@@ -0,0 +1,7 @@
+INSERT INTO authors (name, email) VALUES
+('John Smith', 'john.smith@example.com'),
+('Emily Johnson', 'emily.johnson@example.com'),
+('Michael Brown', 'michael.brown@example.com'),
+('Sophia Davis', 'sophia.davis@example.com'),
+('James Wilson', 'james.wilson@example.com');
+('James Wilson1', 'james.wilson@example1.com');
